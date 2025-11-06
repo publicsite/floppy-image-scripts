2b3c79e1e825196dcc01c9e3b6cd1f5d0a373382
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 00:58:24 +0000

    Update recoverFloppy.sh Set umask

--- recoverFloppy.sh
+++ recoverFloppy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+OLD_UMASK="$(umask)"
+umask 0022
+
 LD_LIBRARY_PATH="${PWD}/disk-utilities/libdisk" disk-analyse "${1}" "${1%????}.imd"
 LD_LIBRARY_PATH="${PWD}/disk-utilities/libdisk" disk-analyse "${1%????}.imd" "${1%????}.img"
 mkdir ${1%????}
@@ -7,3 +10,5 @@ photorec ${1%????}.img
 
 rm ${1%????}.img
 rm ${1%????}.imd
+
+umask "${OLD_UMASK}"
